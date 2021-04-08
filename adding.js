070bd917458f09eb4fb1fb40251974de345deef6
bibhuprasadrout <bibhu.rs@gmail.com>
2021-04-08 11:53:18 +0530

    improved adding.js, created the add function that can add 5 numbers

--- adding.js
+++ adding.js
@@ -1,3 +1,7 @@
-console.log("hello");
-let a = 10;
-console.log(a);
+let num1, num2, num3, num4, num5
+
+let add = function(num1, num2, num3, num4, num5){
+    let sum = num1 + num2 + num3 + num4 + num5
+    console.log(sum)
+}
+add(5, 13, 7, 21, 48)
