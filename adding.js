7a937d54f3984a4a2612b46f8ebc7dc1070bdc83
bibhuprasadrout <bibhu.rs@gmail.com>
2021-04-11 02:31:16 +0530

    improved code in adding.js, added navbar in index.html and accordingly made changes in the style.css file

--- adding.js
+++ adding.js
@@ -1,7 +1,8 @@
 let num1, num2, num3, num4, num5
 
-let add = function(num1, num2, num3, num4, num5){
+let add = function(num1=0, num2=0, num3=0, num4=0, num5=0){
     let sum = num1 + num2 + num3 + num4 + num5
-    console.log(sum)
+    return sum
 }
-add(5, 13, 7, 21, 48)
+let sum = add(5, 13, 7, 21, 48)
+console.log(sum)
